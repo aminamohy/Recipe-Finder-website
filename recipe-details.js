f00caa39c2f8ee305a64d6d28287b532f9161e3c
Shahd132 <162301748+Shahd132@users.noreply.github.com>
2025-04-19 00:53:24 +0200

    Add files via upload

---
+++ recipe-details.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const recipe = JSON.parse(localStorage.getItem("selectedRecipe"));
+
+    document.getElementsByClassName("title")[0].textContent = recipe.name;
+    document.getElementsByClassName("recipe-img")[0].src = recipe.image;
+    document.getElementsByClassName("time")[0].textContent= ` ${recipe.time}`;
+    document.getElementsByClassName("noingredients")[0].textContent = ` ${recipe.noingredients} ingredients`;
+
+    const ingredientList = document.querySelector(".ingredients ul");
+    recipe.ingredients.forEach(ingredient => {
+      const li = document.createElement("li");
+      li.textContent = `${ingredient.name} - ${ingredient.quantity}`;
+      ingredientList.appendChild(li);
+    });
+  
+});
+
+let calorybtn = document.getElementsByClassName("calorycalc")[0];
+calorybtn.onclick = function () {
+  let targetpage = calorybtn.getAttribute("data-target");
+  window.location.href = targetpage;
+};
+
+  
